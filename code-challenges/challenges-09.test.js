6217d7089f77b3f3a33a0f66ab70784683dcffc5
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-07-07 18:18:01 -0700

    #1-4 pass

--- code-challenges/challenges-09.test.js
+++ code-challenges/challenges-09.test.js
@@ -165,8 +165,7 @@ const currentEvents = {
 
 function getCurrentEvents(request, response) {
   // Solution code here...
-  let data = require('currentEvents');
-  let actualData = new Event(data.body.news);
+  let actualData = currentEvents.news.map( item => new Event(item));
   response.status(200).json(actualData);
 
 }
