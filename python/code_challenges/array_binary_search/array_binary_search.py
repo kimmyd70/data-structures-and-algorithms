86b3d359f96f02e8115b0fac7986b0f21ab16711
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-12-19 14:52:45 -0800

    file structure set; RM.md updated

--- python/code_challenges/array_binary_search/array_binary_search.py
+++ python/code_challenges/array_binary_search/array_binary_search.py
@@ -2,13 +2,13 @@ import math
 
 # Roger wanted us to do iteration to see difference
 def brute_force(sorted_array, key):
-    # for i in range(len(sorted_array)):
     for index, value in enumerate(sorted_array): 
         if value == key:
-            return index
             break
+        else:
+            return -1
         
-        
+    return index
 
 def binary_search(list,key):
     lower = 0
