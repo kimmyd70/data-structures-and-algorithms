b17977d1743fb681619cb885bfc27128d5be0fda
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-01-05 13:33:30 -0800

    code and tests passing for edge case testing of one or more empty lists

--- python/code_challenges/ll_zip/ll_zip.py
+++ python/code_challenges/ll_zip/ll_zip.py
@@ -28,20 +28,64 @@ class LinkedList:
             current = current.next
         return output + 'NULL'
     
+    def insert(self, value):
+        node = Node(value) 
+
+        if self.head is not None:
+            node.next = self.head
+        self.head = node
+
     
     def insert_after(self, value, newVal):
-    new_node = Node(newVal)
-    current = self.head
+        new_node = Node(newVal)
+        current = self.head
     
     # checking for empty list and puts at the head
-    if current is None:
-        self.insert(new_node)
+        if current is None:
+            self.insert(new_node)
         return
+        
+        # inserts after a node matching our search value
+        while current is not None:
+            current = current.next    
+            if current.value == value:
+                new_node.next = current.next
+                current.next = new_node
+        return
+
+
+    def zip_lists(list1,list2):
+        error = 'Cannot zip two empty lists'
+        if list1:
+            head1 = list1.head
+            # head2 = list2.head
+        elif list2:
+            head1 = list2.head
+            # head2 = list1.head
+        else:
+            return error
+
+        
+        while list1 or list2:
+            
+        
+            return head1
     
-    # inserts after a node matching our search value
-    while current is not None:
-        current = current.next    
-        if current.value == value:
-            new_node.next = current.next
-            current.next = new_node
-        return
+    # whiteboard flow
+    
+#     define a function with 2 args (LL1, LL2)
+# --- set head to LL1 head (assuming not None)
+# --- set head to LL2head (if LL1 is None)
+# -- throw error when given 2 empty lists
+# -- while LL1 or LL2 (is not None)
+
+# --- check lists and compare lengths
+# --- take care of case where one list reaches None: pivot to "append the rest of the other list"
+# --- insert LL2 node after LL1 current node
+# --- return LL1 head pointer
+
+
+# Edge: 2 empty lists -- will fall out of while loops and print an error
+
+
+# ** uses insert_after method already written
---
+++ python/code_challenges/ll_zip/test_ll_zip.py
@@ -0,0 +1,69 @@
+from ll_zip import LinkedList, Node
+zip_lists = LinkedList.zip_lists
+import pytest
+
+
+#passes
+def test_import():
+    """ proof of life test passes"""
+    assert LinkedList
+    assert Node
+    
+#passes
+def test_empty():
+    """ test 2 empty lists passed in"""
+    L1 = []
+    L2 = []
+    actual = zip_lists(L1,L2)
+    expected = 'Cannot zip two empty lists'
+    assert actual == expected
+    
+#passes
+def test_happy(generate_new_list,generate_new_list2):
+    """ test 2 lists of equal length """
+    actual = zip_lists(generate_new_list,generate_new_list2)
+    expected = generate_new_list.head
+    assert actual == expected
+    
+#passes    
+def test_first_empty(generate_new_list2):
+    """ test list 1 is empty """
+    L1 = []
+    actual = zip_lists(L1,generate_new_list2)
+    expected = generate_new_list2.head
+    assert actual == expected
+    
+#passes    
+def test_second_empty(generate_new_list):
+    """ test list 1 is empty """
+    L2 = []
+    actual = zip_lists(generate_new_list, L2)
+    expected = generate_new_list.head
+    assert actual == expected
+    
+
+@pytest.fixture
+def generate_new_list():
+    """ generates new_list of [1,2,3,4] """
+    node = Node(0)
+    new_list = LinkedList(node)
+    list_length = 0
+    
+    for value in range(1,5):
+        new_list.insert(value)
+        list_length += 1
+    
+    return new_list
+
+@pytest.fixture
+def generate_new_list2():
+    """ generates new_list of [2,3,4,5] """
+    node = Node(0)
+    new_list = LinkedList(node)
+    list_length = 0
+    
+    for value in range(2,6):
+        new_list.insert(value)
+        list_length += 1
+    
+    return new_list
