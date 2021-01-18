f613543fe59dc2fb6937906b5e87309cba84358d
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-01-18 14:30:54 -0800

    multi-bracket-validation folder missing on GH

---
+++ python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -0,0 +1,12 @@
+def bracket_check(string:str)->bool
+    #create empty dictionary
+    
+    #check for '' with if and len(string)
+    #read string via loop for char in len(string)
+        #compare char == opening bracket with 3x if
+            #if found, update dictionary
+            #when cooresponding close found decrease open dictionary -1
+            #if close found first, break loop and return False
+            #return True if count for all keys is 0
+            
+    pass
