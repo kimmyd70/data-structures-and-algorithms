6e9d7d0f006c66185d430f517fe9039055e7783f
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-01-18 20:19:29 -0800

    whiteboard actions coded

--- python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
+++ python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -1,12 +1,40 @@
 def bracket_check(string:str)->bool:
-    #create empty dictionary
+    """function takes in a string and returns boolean True if brackets are 
+    balanced, False if not"""
+    #create empty dictionary with 3 types of opening brackets
+    brackets = {
+        "ro" : 0,
+        "so" : 0,
+        "co" : 0
+    }
     
-    #check for '' with if and len(string)
+    #check for '' with if and len(string); return True because no brackets == balanced
+    string_length = len(string)
+    if string_length == 0:
+        return True
     #read string via loop for char in len(string)
+    for char in string_length:
+        #if close found first, break loop and return False
+        if char == ")" or char == "]" or char == "}" and brackets.ro == 0 and 
+        brackets.so == 0 and brackets.co == 0:
+            return False
         #compare char == opening bracket with 3x if
-            #if found, update dictionary
-            #when cooresponding close found decrease open dictionary -1
-            #if close found first, break loop and return False
-            #return True if count for all keys is 0
+        # #if found, update dictionary
+        if char == "(":
+            brackets.ro +=1
+        elif char == "[":
+            brackets.so +=1
+        elif char == "{":
+            brackets.co +=1
+        #when cooresponding close found decrease open dictionary -1
+        elif char == ")":
+            brackets.ro -=1
+        elif char == "]":
+            brackets.so -=1
+        elif char == "}":
+            brackets.co -=1
             
-    pass
+        #return True if count for all keys is 0
+        if brackets.ro == 0 and brackets.so == 0 and brackets.co == 0:
+            return True
+        
--- python/code_challenges/multi_bracket_validation/test_multi_bracket_validation.py
+++ python/code_challenges/multi_bracket_validation/test_multi_bracket_validation.py
@@ -4,3 +4,9 @@ from multi_bracket_validation import *
 def test_import():
     """ proof of life test passes"""
     assert bracket_check
+    
+#
+def test_empty_string():
+    actual = bracket_check('')
+    expected = True
+    assert actual == expected
