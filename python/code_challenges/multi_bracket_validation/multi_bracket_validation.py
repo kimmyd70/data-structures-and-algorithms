57799083502da9b5189f3d6c371f1ad45d71fc8d
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-01-18 21:05:12 -0800

    happy path does not pass, empty and closer first pass.....false passes on happy path before

--- python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
+++ python/code_challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -10,10 +10,13 @@ def bracket_check(string:str)->bool:
     
     #check for '' with if and len(string); return True because no brackets == balanced
     string_length = len(string)
+    char_list =[char for char in string]
+    print(char_list)
     if string_length == 0:
         return True
     #read string via loop for char in len(string)
-    for char in range(string_length):
+    for char in char_list:
+        print(char)
         #if close found first, break loop and return False
         if char == ")" or char == "]" or char == "}" and brackets["ro"] == 0 and brackets["so"] == 0 and brackets["co"] == 0:
             return False
@@ -21,19 +24,23 @@ def bracket_check(string:str)->bool:
         # #if found, update dictionary
         if char == "(":
             brackets["ro"] +=1
-        elif char == "[":
+        if char == "[":
             brackets["so"] +=1
-        elif char == "{":
+        if char == "{":
             brackets["co"] +=1
         #when cooresponding close found decrease open dictionary -1
-        elif char == ")":
+        if char == ")":
             brackets["ro"] -=1
-        elif char == "]":
+        if char == "]":
             brackets["so"] -=1
-        elif char == "}":
+        if char == "}":
             brackets["co"] -=1
+            
+        print(brackets)
             
     #return True if count for all keys is 0
     if brackets["ro"] == 0 and brackets["so"] == 0 and brackets["co"] == 0:
         return True
+    else:
+        return False
         
--- python/code_challenges/multi_bracket_validation/test_multi_bracket_validation.py
+++ python/code_challenges/multi_bracket_validation/test_multi_bracket_validation.py
@@ -11,20 +11,38 @@ def test_empty_string():
     expected = True
     assert actual == expected
 
-#passes
+#
 def test_basic_string():
     actual = bracket_check('()[]{}')
     expected = True
     assert actual == expected
     
-# passes
+# 
 def test_balanced_string():
     actual = bracket_check('(aslj(l;al))[yes]')
     expected = True
     assert actual == expected
     
-#passes
+# 
+def test_string_with_spaces():
+    actual = bracket_check('(  )wr[pu]')
+    expected = True
+    assert actual == expected
+
+# 
+def test_unbalanced_string1():
+    actual = bracket_check('(')
+    expected = False
+    assert actual == expected
+    
+# passes
 def test_unbalanced_string():
     actual = bracket_check('(wrpu{al;j}')
+    expected = False
+    assert actual == expected
+    
+# passes
+def test_closer_first():
+    actual = bracket_check('}{al;j')
     expected = False
     assert actual == expected
