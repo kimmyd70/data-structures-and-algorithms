e9948244dfbedf663075a6eb27879cf61a06570f
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-01-30 14:59:17 -0800

    instantiate BT and BST pass; BT with one node passes; BST with one node does not pass

--- python/code_challenges/tree/test_tree.py
+++ python/code_challenges/tree/test_tree.py
@@ -1,18 +1,30 @@
 from tree import Node, BinaryTree, BinarySearchTree
 
 
-# 1. Passes--Can successfully instantiate an empty tree
-def test_import():
+# 1. Passes--Can successfully instantiate an empty BinaryTree
+def test_create_BT():
     assert Node
     assert BinaryTree
     
-# 2. Passes--Can successfully instantiate a tree with a single root node
-def test_one_node_tree():
+# 1a. Passes--Can successfully instantiate an empty BinarySearchTree
+def test_create_BST():
+    assert Node
+    assert BinarySearchTree
+
+    
+# 2. Passes--Can successfully instantiate a BinaryTree with a single root node
+def test_one_node_BT():
     node = Node(7)
     actual = str(BinaryTree(node))
-    expected = f'{{ 7 }} ->'
+    expected = f'BT left: {{ 7 }} ->  and right: {{ 7 }} -> '
+    assert actual == expected
     
-    
+# 2a. Can successfully instantiate a BinaryTree with a single root node
+def test_one_node_BST():
+    node = Node(9)
+    actual = str(BinarySearchTree(node))
+    expected = f'{{ 9 }} ->'
+
 # 3. Can successfully add a left child and right child to a single root node
 # 4. Can successfully return a collection from a preorder traversal
 # 5. Can successfully return a collection from an inorder traversal
--- python/code_challenges/tree/tree.py
+++ python/code_challenges/tree/tree.py
@@ -1,4 +1,5 @@
-
+class InvalidOperationError(BaseException):
+    pass
 
 
 class Node:
@@ -6,13 +7,14 @@ class Node:
         self.value = value
         self.left = left
         self.right = right
-        raise('something went wrong with Node instantiation')
         
 class BinaryTree:
     def __init__(self, root = None):
         self.root = root
+
         
     def __str__(self):
+
         output_left = ''
         output_right = ''
         current = self.root
@@ -25,31 +27,55 @@ class BinaryTree:
         while current:
             output_right += f'{{ {current.value} }} -> '
             current = current.right
-        return f'left: {output_left} and right: {output_right}'
+        return f'BT left: {output_left} and right: {output_right}'
         
         
     def pre_order():
-        #raise('something went wrong with pre_order operation')
+        # if not self:
+        #     raise InvalidOperationError('something went wrong with pre_order operation')
+
         pass
     
     def in_order():
-        #raise('something went wrong with in_order operation')
+        # if not self:
+        #     raise InvalidOperationError('something went wrong with in_order operation')
+
         pass
 
     def post_order():
-        #raise('something went wrong with post_order operation')
+        # if not self:
+        #     raise InvalidOperationError('something went wrong with post_order operation')
         pass
         
 class BinarySearchTree(BinaryTree):
-    def __init__():
-        # BinaryTree()
-        pass
+    def __init__(self):
+        BinaryTree(self)
+
+    def __str__(self):
+
+        output_left = ''
+        output_right = ''
+        current = self.root
+        while current:
+            output_left += f'{{ {current.value} }} ->'
+            current = current.left
+            
+        current = self.root
+        
+        while current:
+            output_right += f'{{ {current.value} }} ->'
+            current = current.right
+        return f'BST left: {output_left} and right: {output_right}'
+        
+
 
     def add():
-        #raise('something went wrong with add operation')
+        # if not self:
+        #     raise InvalidOperationError('something went wrong with add operation')
         pass
     
     def contains():
-        #raise('something went wrong with contains operation')
+        # if not self:
+        #     raise InvalidOperationError('something went wrong with post_order operation')
         pass
     
