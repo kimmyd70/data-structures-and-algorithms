2abea78c0a61feca3531131ff1943b925b18f02b
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-01-31 14:46:48 -0800

    can add each kid, but needs to be squished into one

--- python/code_challenges/tree/test_tree.py
+++ python/code_challenges/tree/test_tree.py
@@ -1,5 +1,7 @@
 from tree import Node, BinaryTree 
 from tree import BinarySearchTree
+import pytest
+
 
 # 15 tests total
 # 1. Passes--Can successfully instantiate an empty BinaryTree
@@ -12,29 +14,47 @@ def test_create_BST():
     assert Node
     assert BinarySearchTree
 
-    
 # 2. Passes--Can successfully instantiate a BinaryTree with a single root node
 def test_one_node_BT():
-    node = Node(7)
-    actual = str(BinaryTree(node))
-    expected = 'left: [7] and right: [7]'
+    actual = str(BinaryTree('a'))
+    expected = 'root:a, left:None, right:None'
     assert actual == expected
     
-# 2a. Passes--Can successfully instantiate a BinarySearchTree with a single root node
-def test_one_node_BST():
-    node = Node(9)
-    actual = str(BinarySearchTree(node))
-    expected = 'left: [9] and right: [9]'
+# # 2a. Passes--Can successfully instantiate a BinarySearchTree with a single root node
+# def test_one_node_BST():
+#     actual = str(BinarySearchTree(11))
+#     expected = 11
+#     assert actual == expected
+    
+# 3. ***** Both pass, but need to be smooshed together ******
+# Can successfully add a left child and right child to a single root node
+def test_BT_add_left_kid():
+    
+    actual = str(BinaryTree.add_left('a','b'))
+    expected = 'root:a, left:b, right:None'
     assert actual == expected
 
-# 3. Can successfully add a left child and right child to a single root node
-# def test_BT_add_two_kids():
+def test_BT_add_right_kid():
+    
+    actual = str(BinaryTree.add_right('a','c'))
+    expected = 'root:a, left:None, right:c'
+    assert actual == expected
+    
+# def test_add_both_kids():
+    
+#     actual = str(BinaryTree.add_both('a', 'None' ,'b'))
+#     expected = 'root:a, left:None, right:None'
+#     assert actual == expected
 
 # 3a. Can successfully add a left child and right child to a single root node
 # def test_BST_add_two_kids():
 
 
 # 4. Can successfully return a collection from a pre_order traversal
+# def test_pre_order(create_tree):
+    
+#     actual = str(BinaryTree.pre_order(create_tree))
+#     expected = [11,10,13]
 
 # 4.a Can successfully return a True Boolean for BST pre_order search of an input
 
@@ -54,13 +74,14 @@ def test_one_node_BST():
 
 
 
-@pytest.fixture
-def create_tree():
-    """ instantiate and fill a small BT--see visual in images """
-    tree = BinaryTree(Node(11))
-    tree.root.left = Node(10)
-    tree.root.left.left = Node(9)
-    tree.root.left.right = Node(12)
-    tree.root.right = Node(13)
-    tree.root.right.left = Node(7)
-    tree.root.right.right = Node(15)
+# @pytest.fixture
+# def create_tree():
+#     """ instantiate and fill a small BT--see visual in images """
+#     tree = BinaryTree(Node(11))
+#     tree.add = Node(10, left)
+#     # tree.root.left.left = Node(9)
+#     # tree.root.left.right = Node(12)
+#     tree.add = Node(13, right)
+#     # tree.root.right.left = Node(7)
+#     # tree.root.right.right = Node(15)
+#     return tree
--- python/code_challenges/tree/tree.py
+++ python/code_challenges/tree/tree.py
@@ -3,94 +3,141 @@ class InvalidOperationError(BaseException):
 
 
 class Node:
-    def __init__ (self, value = None, left = None, right = None):
+    def __init__ (self, value = None):
         self.value = value
-        self.left = left
-        self.right = right
         
 class BinaryTree:
-    def __init__(self, root = None):
-        self.root = root
-
+    def __init__(self, root = None, left = None, right = None):
+        """ takes in up to 3 values creating Nodes with each value"""
+        self.root = Node(root)
+        self.left = Node(left)
+        self.right = Node(right)
         
     def __str__(self):
-        """ function used to test instantiation of one-node BT and BST"""
-        # baby steps (TDD)
-
-        output_left = []
-        output_right = []
-        current = self.root
-        while current:
-            output_left.append(current.value)
-            current = current.left
-            
-        current = self.root
+        return str(f'root:{self.root.value}, left:{self.left.value}, right:{self.right.value}')
+    
+    def add_left (root, left = None):
+        """ takes in a root and adds value to left; instantiates a sub-tree"""
+        right = None
+        new_tree_left = BinaryTree(root, left, right)
+        return new_tree_left
         
-        while current:
-            output_right.append(current.value)
-            current = current.right
-        return f'left: {output_left} and right: {output_right}'
+    def add_right (root,right):
+        """ takes in a root and adds value to left; instantiates a sub-tree"""
+        left = None
+        new_tree_right = BinaryTree(root, left, right)
+        return new_tree_right
+
+    # def __str__(self):
+    #     """ function used to test instantiation of one-node BT and BST"""
+    #     # baby steps (TDD)
+
+    #     output_left = []
+    #     output_right = []
+    #     current = self.root
+    #     while current:
+    #         output_left.append(current.value)
+    #         current = current.left
+            
+    #     current = self.root
+        
+    #     while current:
+    #         output_right.append(current.value)
+    #         current = current.right
+    #     return f'left: {output_left} and right: {output_right}'
         
         
     def pre_order(self, root = None):
-        """ function prints root then left traverse """
-        if not self:
-            raise InvalidOperationError('something went wrong with creating a tree')
-            return
-        if not root:
-            raise InvalidOperationError('your tree is empty')
-            return
+    #     """ function prints root then left traverse """
+    #     collection = []
+    #     if not self:
+    #         raise InvalidOperationError('something went wrong with creating a tree')
+    #         return
+    #     if not root:
+    #         raise InvalidOperationError('your tree is empty')
+    #         return
         
-        def traverse(root):
-            print(root.value)
-            if root.left:
-                traverse(root.left)
+    #     def traverse(root):
+    #         collection.append(root)
+    #         return(root.value)
+    #         if root.left:
+    #             traverse(root.left)
             
-            if root.right:
-                traverse(root.right)
-        traverse(self.root)
+    #         if root.right:
+    #             traverse(root.right)
+    #     traverse(self.root)
+    #     return collection
+        
 
         pass
     
     def in_order(self, root = None):
-        """ function left traverse then prints root  """
+        # """ function left traverse then prints root  """
         
-        if not self:
-            raise InvalidOperationError('something went wrong with creating a tree')
-            return
-        if not root:
-            raise InvalidOperationError('your tree is empty')
-            return
+        # if not self:
+        #     raise InvalidOperationError('something went wrong with creating a tree')
+        #     return
+        # if not root:
+        #     raise InvalidOperationError('your tree is empty')
+        #     return
         
-        def traverse(root):
-            if root.left:
-                traverse(root.left)
-            print(root.value)
+        # def traverse(root):
+        #     if root.left:
+        #         traverse(root.left)
+        #     return(root.value)
             
-            if root.right:
-                traverse(root.right)
-        traverse(self.root)
-        # pass
+        #     if root.right:
+        #         traverse(root.right)
+        # traverse(self.root)
+        pass
 
     def post_order(self, root = None):
-        if not self:
-            raise InvalidOperationError('something went wrong with creating a tree')
-            return
-        if not root:
-            raise InvalidOperationError('your tree is empty')
-            return
+        """ function does left traverse, right traverse, then print root """
+
+        # if not self:
+        #     raise InvalidOperationError('something went wrong with creating a tree')
+        #     return
+        # if not root:
+        #     raise InvalidOperationError('your tree is empty')
+        #     return
+        
+        # def traverse(root):
+        #     if root.left:
+        #         traverse(root.left)
+            
+        #     if root.right:
+        #         traverse(root.right)
+                
+        #     return(root.value)
+        # traverse(self.root)
+        pass
+
         
 class BinarySearchTree(BinaryTree):
-    def __init__(self, root = None):
-        self.root = root
+    def __init__(self, root = None, left = None, right = None):
+        # self.root = root
+        # self.left = left
+        # self.right = right
+        pass
         
-    def add(self, value):
+    def add(self, value = None, position = None):
+        # """ Input = value:str or integer, position:string)
+        #     Output = Node at position with .value = value """
         # if not self:
-        #     raise InvalidOperationError('something went wrong with add operation')
+        #     raise InvalidOperationError('something went wrong with creating a tree')
+        #     return
+        # if not root:
+        #     raise InvalidOperationError('your tree is empty')
+        #     return
+        # new_node = BinarySearchTree(Node(value), position)
         pass
     
     def contains(self,value):
         # if not self:
-        #     raise InvalidOperationError('something went wrong with post_order operation')
+        #     raise InvalidOperationError('something went wrong with creating a tree')
+        #     return
+        # if not root:
+        #     raise InvalidOperationError('your tree is empty')
+        #     return
         pass
     
