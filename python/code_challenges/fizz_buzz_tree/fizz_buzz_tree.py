72305d0ef9d7ba3bcf643077f30180ab02b4047a
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-02-08 20:23:23 -0800

    all the parts and research are crammed into .py file for later reference

--- python/code_challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ python/code_challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -14,32 +14,72 @@
 
 # 3. Return a new tree.
 
-# ****  Look up early class demo with elegant code
-def fizz_buzz(num):
-    if not num % 15:
-        return 'FizzBuzz'
-    elif num % 3 == 0:
-        return 'Fizz'
-    elif num % 5 == 0:
-        return 'Buzz'
-    else:
-        return str(num)
+####################
+# reference: https://stackoverflow.com/questions/23153319/n-ary-tree-in-python
+class Node_k(obj):
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+        self.children = []
+        
+    def add_child(self, obj):
+        self.children.append(obj)
+        
+class K_aryTree:
+    def __init__(self, root = None):
+        self.root = root
+                    
+        
 
-# def fizz_buzz_tree(self):
-#     copy = Tree()
-#     root = self.root
-#     if root %15:
-#        root.value = FizzBuzz
-#         copy.root.value = FizzBuzz
-#     elif root %3:
-#        root.value = FizzBuzz
-#         copy.root.value = Fizz
-#     elif root %5:
-#        root.value = Buzz
-#         copy.root.value = Buzz
-#    else:
-#        root.value = FizzBuzz
-#         copy.root.value = Fizz
+    # basic FB function
+    def fizz_buzz(num):
+        if not num % 15:
+            return 'FizzBuzz'
+        elif num % 3 == 0:
+            return 'Fizz'
+        elif num % 5 == 0:
+            return 'Buzz'
+        else:
+            return str(num)
 
-#     def traverse
-#       *** review code from tree implementation***
+    # def fizz_buzz_tree(self):
+    #     copy = Tree()
+    #     root = self.root
+    #     if root %15:
+    #        root.value = FizzBuzz
+    #         copy.root.value = FizzBuzz
+    #     elif root %3:
+    #        root.value = FizzBuzz
+    #         copy.root.value = Fizz
+    #     elif root %5:
+    #        root.value = Buzz
+    #         copy.root.value = Buzz
+    #    else:
+    #        root.value = FizzBuzz
+    #         copy.root.value = Fizz
+
+    #     def traverse
+    #       *** review code from tree implementation***
+    # https://cs.stackexchange.com/questions/44820/what-does-pre-post-and-in-order-walk-mean-for-a-n-ary-tree
+
+        # def pre_order(self, root = None):
+        #     """ function prints root then left traverse """
+        #     # replace 79-83 ifs with for loop over list of children
+        #     collection = []
+        #     if not self:
+        #         raise InvalidOperationError('something went wrong with creating a tree')
+        #         return
+        #     if not root:
+        #         raise InvalidOperationError('your tree is empty')
+        #         return
+            
+        #     def traverse(root):
+        #         collection.append(root.value)
+        #         # return(root.value)
+        #         # if root.left:
+        #         #     traverse(root.left)
+                
+        #         # if root.right:
+        #         #     traverse(root.right)
+        #     traverse(self.root)
+        #     return collection
