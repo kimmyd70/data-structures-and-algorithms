8acc6b7cd17ad1ba475115d99cd75a2bc016e513
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-03-03 20:44:47 -0800

    merge_sort BLOG.md complete

---
+++ python/code_challenges/merge_sort/test_merge_sort.py
@@ -0,0 +1,25 @@
+import pytest
+from merge_sort import *
+
+#### all tests pass ###
+
+# @pytest.mark.skip("pending")
+def test_merge_sort_happy():
+    array = [8,4,23,42,16,15]
+    actual = merge_sort(array)[0]
+    expected = [4,8,15,16,23,42]
+    assert actual == expected
+    
+# @pytest.mark.skip("pending")
+def test_merge_sort_dupes():
+    array = [8,4,4,4,16,5]
+    actual = imerge_sort(array)[0]
+    expected = [4,4,4,5,8,16]
+    assert actual == expected
+    
+# @pytest.mark.skip("pending")
+def test_merge_sort_empty():
+    array = []
+    actual = merge_sort(array)
+    expected = 'cannot sort an empty array'
+    assert actual == expected
