986fec7e480c5681a992719e0ae26ac2c2102769
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2021-03-03 21:12:42 -0800

    merge_sort coded but not working properly acc to printed output

--- python/code_challenges/merge_sort/merge_sort.py
+++ python/code_challenges/merge_sort/merge_sort.py
@@ -1,33 +1,55 @@
-ALGORITHM Mergesort(arr)
-    DECLARE n <-- arr.length
-           
-    if n > 1
-      DECLARE mid <-- n/2
-      DECLARE left <-- arr[0...mid]
-      DECLARE right <-- arr[mid...n]
-      // sort the left side
-      Mergesort(left)
-      // sort the right side
-      Mergesort(right)
-      // merge the sorted left and right sides together
-      Merge(left, right, arr)
 
-ALGORITHM Merge(left, right, arr)
-    DECLARE i <-- 0
-    DECLARE j <-- 0
-    DECLARE k <-- 0
 
-    while i < left.length && j < right.length
-        if left[i] <= right[j]
-            arr[k] <-- left[i]
-            i <-- i + 1
-        else
-            arr[k] <-- right[j]
-            j <-- j + 1
+def merge_sort(arr):
+    n = len(arr)
+    left = []
+    right = []
+
+    if n > 1:
+        mid = n//2
+        
+        for index in range(0,mid):
+            left.append(arr[index])
+        
+        for index in range(mid,n-1):
+            right.append(arr[index])
             
-        k <-- k + 1
+        #   sort the left side
+        merge_sort(left)
+        print(left)
+        #   sort the right side
+        merge_sort(right)
+        print(right)
+        #   merge the sorted left and right sides together
+        merge(left, right, arr)
+    
 
-    if i = left.length
-       set remaining entries in arr to remaining values in right
-    else
-       set remaining entries in arr to remaining values in left
+def  merge(left, right, arr):
+    i = 0
+    j = 0
+    k = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+            
+        k += 1
+
+    if i == len(left):
+        # set remaining entries in arr to remaining values in right
+        for index in range (i,len(right)):
+            arr.append(right[index])
+    else:
+        # set remaining entries in arr to remaining values in left
+        for index in range (i,len(left)):
+            arr.append(left[index])
+
+    return arr
+
+if __name__ == "__main__":
+    array = [8,4,23,42,16,15]
+    merge_sort(array)
